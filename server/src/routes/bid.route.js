85c45e6171e4dee7bde4c73d7c324cb78baf7390
jatin <jatinarya578@gmail.com>
2026-01-12 23:04:11 +0530

    Fixed file case sensitivity error

--- server/src/routes/bid.route.js
+++ server/src/routes/bid.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createBid, getFreelancerBids } from "../controller/bid.Controller.js";
+import { createBid, getFreelancerBids } from "../controller/bid.contoller.js"
 import { hireFreelancer } from "../controller/hire.controller.js"; 
 import { protect } from "../middleware/authMiddleware.js";
 
